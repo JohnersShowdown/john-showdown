283c41160be875bf9504256790417846b3762405
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2026-05-22 09:17:35 -0700

    Fix conditional check for mega evolution data

--- data/mods/mixandmega/scripts.ts
+++ data/mods/mixandmega/scripts.ts
@@ -12,7 +12,8 @@ export const Scripts: ModdedBattleScriptsData = {
 			if (item.isNonstandard === "Past" || item.isNonstandard === "Future") this.modData('Items', i).isNonstandard = null;
 			if (item.megaStone) {
 				for (const megaEvo of Object.values(item.megaStone)) {
-					this.modData('FormatsData', this.toID(megaEvo)).isNonstandard = null;
+					if (this.modData('FormatsData', this.toID(megaEvo))
+						this.modData('FormatsData', this.toID(megaEvo)).isNonstandard = null;
 				}
 			}
 		}
